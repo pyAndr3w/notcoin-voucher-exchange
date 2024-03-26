7daec0c11cd016ce2088ac3aa493fb9dc01b3648
Andrew Gutarev <gutarev01@gmail.com>
2024-03-26 19:51:32 +0500

    add config constructor

--- wrappers/VoucherExchange.ts
+++ wrappers/VoucherExchange.ts
@@ -1,9 +1,27 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
 
-export type VoucherExchangeConfig = {};
+export type VoucherExchangeConfig = {
+    admin: Address,
+    jettonRoot: Address,
+    map100m: Cell,
+    minIdx: bigint,
+    maxIdx: bigint
+};
 
 export function voucherExchangeConfigToCell(config: VoucherExchangeConfig): Cell {
-    return beginCell().endCell();
+    return beginCell()
+          .storeAddress(config.admin)
+          .storeAddress(null)
+          .storeUint(0, 1)
+          .storeAddress(config.jettonRoot)
+          .storeCoins(0)
+          .storeRef(beginCell()
+                   .storeUint(1,1)
+                   .storeRef(config.map100m)
+                   .storeUint(config.minIdx, 64)
+                   .storeUint(config.maxIdx, 64)
+                   .endCell())
+          .endCell();
 }
 
 export class VoucherExchange implements Contract {
